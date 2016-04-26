87de4f3afcda27fd54b8b3fe3f3d38210c79ff92
oomusou <oomusou@gmail.com>
2016-04-26 11:08:38 +0800

    修改 gulpfile.js，啟動 Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
+elixir(function (mix) {
     mix.sass('app.scss');
+
+    mix.browserSync({
+        files: ['app/**/*', 'public/**/*', 'resources/views/**/*'],
+        port: 5000,
+        proxy: 'localhost:8000'
+    });
 });
